ed726e31c9c19d68bce85fb1723c1ab640f90bf1
nanosilva <seba_nanosilva@hotmail.com>
2022-03-19 15:44:18 -0300

    componente experiencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { allIcons,NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
